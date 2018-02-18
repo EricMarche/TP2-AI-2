0ab5b6c37ca87e7a87fc92569350a2b83f03d61b
EricMarche <eric.marchessault@gmail.com>
2018-02-18 16:49:28 -0500

    initial commit

---
+++ Code/entrainer_tester.py
@@ -0,0 +1,43 @@
+import numpy as np
+import sys
+import load_datasets
+import BayesNaif # importer la classe du classifieur bayesien
+import Knn # importer la classe du Knn
+#importer dautres fichiers et classes si vous en avez developpes
+
+#-*- coding: utf-8 -*-
+# encoding=utf8
+
+"""
+Cest le fichier main duquel nous allons tout lancer
+Vous allez dire en commentaire cest quoi les parametres que vous avez utilises
+En gros, vous allez :
+1- Initialiser votre classifieur avec ses parametres
+2- Charger les datasets
+3- Entrainer votre classifieur
+4- Le tester
+
+"""
+
+# Initializer vos parametres
+
+
+
+
+# Initializer/instanciez vos classifieurs avec leurs parametres
+
+
+
+
+
+# Charger/lire les datasets
+load_datasets.load_iris_dataset(0.5)
+
+
+# Entrainez votre classifieur
+
+
+
+
+
+# Tester votre classifieur
